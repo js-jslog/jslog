ffa0ac2fa14349ed86cd5490f831ef52c917bb45
js-jslog <josephsinfield@yahoo.com>
2017-11-20 23:27:35 +0000

    adding clean step before html build

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,16 @@
 const gulp = require('gulp');
+const clean = require('gulp-clean');
 const handlebars = require('gulp-compile-handlebars');
 const rename = require('gulp-rename');
+
+gulp.task('clean', () => {
+    return gulp.src('./pages', {
+        read: false
+    })
+    .pipe(clean({}, {
+        force: true
+    }))
+});
 
 gulp.task('html', () => {
     return gulp.src('./src/pages/**/*.hbs')
