281a77485971b94d472c86c6a38163070dce7c8c
js-jslog <jhs4jbs@hotmail.co.uk>
2017-12-20 17:43:50 +0000

    needed to expand the functionality to complete a whole row

--- src/jsx/article-box-row.jsx
+++ src/jsx/article-box-row.jsx
@@ -1,15 +1,36 @@
 import ArticleBox from './article.jsx';
 //import ReactDOM from 'react-dom';
 
-const dataArray = [
+const dataArray1 = 
     {
-        className: "articles-link-box",
         href: "/articles/modularisation-without-obstruction",
         imgSrc: "/images/hero/modularisation-without-obstruction.jpg",
-    },
-];
+        articleTitle: "Modularisation without obstruction",
+        blurb: "Overview of the article. Probably the same text which should appear in the hero image under the title when I get round to that",
+    }
+;
+const dataArray2 = 
+    {
+        href: "/articles/ultimate-pseudo-classical-inheritance",
+        imgSrc: "/images/hero/ultimate-pseudo-classical-inheritance.jpg",
+        articleTitle: "The ultimate pseudo classical inheritance pattern",
+        blurb: "Overview of the article. Probably the same text which should appear in the hero image under the title when I get round to that",
+    }
+;
+const dataArray3 = 
+    {
+        href: "/articles/loops-or-array-methods",
+        imgSrc: "images/hero/loops-or-array-methods.jpg",
+        articleTitle: "Loops or array methods",
+        blurb: "Overview of the article. Probably the same text which should appear in the hero image under the title when I get round to that",
+    }
+;
 
 ReactDOM.render(
-  <ArticleBox data={dataArray} />,
-  document.getElementById('article1')
+  <div className="row">
+    <ArticleBox data={dataArray1} />
+    <ArticleBox data={dataArray2} />
+    <ArticleBox data={dataArray3} />
+  </div>,
+  document.getElementById('row1')
 );
--- src/jsx/article.jsx
+++ src/jsx/article.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 class Article extends React.Component {
     render() {
         return <div className="col s12 m4">
-          <a className="articles-link-box" href={this.props.href}>
-            <img src="/images/hero/modularisation-without-obstruction.jpg" alt="this needs defining" style={{width: "100%"}} />
-            <h5 className="center">Modularisation without obstruction</h5>
-            <p className="light">Overview of the article. Probably the same text which should appear in the hero image under the title when I get round to that</p>
+          <a className="articles-link-box" href={this.props.data.href}>
+            <img src={this.props.data.imgSrc} alt="this needs defining" style={{width: "100%"}} />
+            <h5 className="center">{this.props.data.articleTitle}</h5>
+            <p className="light">{this.props.data.blurb}</p>
           </a>
       </div>
     };
