341294f73712f8d88302499ce3c775ea71c5da98
js-jslog <jhs4jbs@hotmail.co.uk>
2017-12-20 17:54:36 +0000

    putting data in to a single array on the way to iterating

--- src/jsx/article-box-row.jsx
+++ src/jsx/article-box-row.jsx
@@ -1,36 +1,32 @@
 import ArticleBox from './article.jsx';
 //import ReactDOM from 'react-dom';
 
-const dataArray1 = 
+const data_array = [
     {
         href: "/articles/modularisation-without-obstruction",
         imgSrc: "/images/hero/modularisation-without-obstruction.jpg",
         articleTitle: "Modularisation without obstruction",
         blurb: "Overview of the article. Probably the same text which should appear in the hero image under the title when I get round to that",
-    }
-;
-const dataArray2 = 
+    },
     {
         href: "/articles/ultimate-pseudo-classical-inheritance",
         imgSrc: "/images/hero/ultimate-pseudo-classical-inheritance.jpg",
         articleTitle: "The ultimate pseudo classical inheritance pattern",
         blurb: "Overview of the article. Probably the same text which should appear in the hero image under the title when I get round to that",
-    }
-;
-const dataArray3 = 
+    },
     {
         href: "/articles/loops-or-array-methods",
         imgSrc: "images/hero/loops-or-array-methods.jpg",
         articleTitle: "Loops or array methods",
         blurb: "Overview of the article. Probably the same text which should appear in the hero image under the title when I get round to that",
-    }
-;
+    },
+];
 
 ReactDOM.render(
   <div className="row">
-    <ArticleBox data={dataArray1} />
-    <ArticleBox data={dataArray2} />
-    <ArticleBox data={dataArray3} />
+    <ArticleBox data={data_array[0]} />
+    <ArticleBox data={data_array[1]} />
+    <ArticleBox data={data_array[2]} />
   </div>,
   document.getElementById('row1')
 );
