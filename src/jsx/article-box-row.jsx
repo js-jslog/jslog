998fa6a5bb7b285396d166e058b57f2bc348f2af
js-jslog <jhs4jbs@hotmail.co.uk>
2017-12-21 00:11:03 +0000

    need to keep the data as high up the model as possible

--- src/jsx/article-box-row.jsx
+++ src/jsx/article-box-row.jsx
@@ -1,30 +1,9 @@
 import ArticleBox from './article.jsx';
 //import ReactDOM from 'react-dom';
 
-const data_array = [
-    {
-        href: "/articles/modularisation-without-obstruction",
-        imgSrc: "/images/hero/modularisation-without-obstruction.jpg",
-        articleTitle: "Modularisation without obstruction",
-        blurb: "Overview of the article. Probably the same text which should appear in the hero image under the title when I get round to that",
-    },
-    {
-        href: "/articles/ultimate-pseudo-classical-inheritance",
-        imgSrc: "/images/hero/ultimate-pseudo-classical-inheritance.jpg",
-        articleTitle: "The ultimate pseudo classical inheritance pattern",
-        blurb: "Overview of the article. Probably the same text which should appear in the hero image under the title when I get round to that",
-    },
-    {
-        href: "/articles/loops-or-array-methods",
-        imgSrc: "images/hero/loops-or-array-methods.jpg",
-        articleTitle: "Loops or array methods",
-        blurb: "Overview of the article. Probably the same text which should appear in the hero image under the title when I get round to that",
-    },
-];
-
 class ArticleRow extends React.Component {
     render() {
-        let articles = data_array.map(article_info => {
+        let articles = this.props.data_array.map(article_info => {
             return <ArticleBox data={article_info} />
         });
         return <div className="row">
--- src/jsx/articles-page-content.jsx
+++ src/jsx/articles-page-content.jsx
@@ -1,6 +1,27 @@
 import ArticleRow from './article-box-row.jsx';
 
+const data_array = [
+    {
+        href: "/articles/modularisation-without-obstruction",
+        imgSrc: "/images/hero/modularisation-without-obstruction.jpg",
+        articleTitle: "Modularisation without obstruction",
+        blurb: "Overview of the article. Probably the same text which should appear in the hero image under the title when I get round to that",
+    },
+    {
+        href: "/articles/ultimate-pseudo-classical-inheritance",
+        imgSrc: "/images/hero/ultimate-pseudo-classical-inheritance.jpg",
+        articleTitle: "The ultimate pseudo classical inheritance pattern",
+        blurb: "Overview of the article. Probably the same text which should appear in the hero image under the title when I get round to that",
+    },
+    {
+        href: "/articles/loops-or-array-methods",
+        imgSrc: "images/hero/loops-or-array-methods.jpg",
+        articleTitle: "Loops or array methods",
+        blurb: "Overview of the article. Probably the same text which should appear in the hero image under the title when I get round to that",
+    },
+];
+
 ReactDOM.render(
-  <ArticleRow />,
+  <ArticleRow data_array={data_array} />,
   document.getElementById('row1')
 );
