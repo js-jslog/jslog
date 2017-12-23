d27d1bca6481aafbfc14997264888b0dd17dfd46
js-jslog <jhs4jbs@hotmail.co.uk>
2017-12-23 13:51:18 +0000

    ultimate-pseduo-classical-inheritance page is now a react component

--- src/react-components/PageBodyFactory.jsx
+++ src/react-components/PageBodyFactory.jsx
@@ -1,6 +1,7 @@
 import ArticlesPage from './pages/ArticlesPage.jsx';
 import IndexPage from './pages/IndexPage.jsx';
 import BooleanFunctionPage from './pages/BooleanFunctionPage.jsx';
+import UltimatePseudoClassicalInheritancePage from './pages/UltimatePseudoClassicalInheritancePage.jsx';
 
 class PageBodyFactory extends React.Component {
     render() {
@@ -12,6 +13,10 @@ class PageBodyFactory extends React.Component {
             contentTag = <ArticlesPage />;
         } else if (pathname === "/articles/boolean-function") {
             contentTag = <BooleanFunctionPage />
+        } else if (pathname === "/articles/ultimate-pseudo-classical-inheritance") {
+            contentTag = <UltimatePseudoClassicalInheritancePage />
+        } else {
+            contentTag = <IndexPage />
         }
         return contentTag;
     };
