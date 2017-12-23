40f7f2dafdf63259a731619d6e45e298525626c3
js-jslog <jhs4jbs@hotmail.co.uk>
2017-12-23 15:07:08 +0000

    the two pillars of javascript page is now a react component

--- src/react-components/PageBodyFactory.jsx
+++ src/react-components/PageBodyFactory.jsx
@@ -2,6 +2,7 @@ import ArticlesPage from './pages/ArticlesPage.jsx';
 import IndexPage from './pages/IndexPage.jsx';
 import BooleanFunctionPage from './pages/BooleanFunctionPage.jsx';
 import UltimatePseudoClassicalInheritancePage from './pages/UltimatePseudoClassicalInheritancePage.jsx';
+import TheTwoPillarsOfJavascriptPage from './pages/TheTwoPillarsOfJavascriptPage.jsx';
 
 class PageBodyFactory extends React.Component {
     render() {
@@ -15,6 +16,8 @@ class PageBodyFactory extends React.Component {
             contentTag = <BooleanFunctionPage />
         } else if (pathname === "/articles/ultimate-pseudo-classical-inheritance") {
             contentTag = <UltimatePseudoClassicalInheritancePage />
+        } else if (pathname === "/articles/the-two-pillars-of-javascript") {
+            contentTag = <TheTwoPillarsOfJavascriptPage />
         } else {
             contentTag = <IndexPage />
         }
