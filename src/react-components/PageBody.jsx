cc8ea4218d9ba19a8fd5a577d199c24d5dbc791e
js-jslog <jhs4jbs@hotmail.co.uk>
2017-12-24 23:19:38 +0000

    making use of the PageContentFactory again

--- src/react-components/PageBody.jsx
+++ src/react-components/PageBody.jsx
@@ -1,96 +1,81 @@
 import NavigationBar from './NavigationBar.jsx';
 import Footer from './Footer.jsx';
 import HeroBanner from './HeroBanner.jsx';
-
-import ArticlesPage from './pages/ArticlesPage.jsx';
-import IndexPage from './pages/IndexPage.jsx';
-import BooleanFunctionPage from './pages/BooleanFunctionPage.jsx';
-import UltimatePseudoClassicalInheritancePage from './pages/UltimatePseudoClassicalInheritancePage.jsx';
-import TheTwoPillarsOfJavascriptPage from './pages/TheTwoPillarsOfJavascriptPage.jsx';
-import TheObjectObjectPage from './pages/TheObjectObjectPage.jsx';
-import PrefixVsPostfixIncrementUnaryOperatorsPage from './pages/PrefixVsPostfixIncrementUnaryOperatorsPage.jsx';
-import NumberConversionPage from './pages/NumberConversionPage.jsx';
-import ModularisationWithoutObstructionPage from './pages/ModularisationWithoutObstructionPage.jsx';
-import LoopsOrArrayMethodsPage from './pages/LoopsOrArrayMethodsPage.jsx';
-import KeywordsAndReservedWordsInJavascriptPage from './pages/KeywordsAndReservedWordsInJavascriptPage.jsx';
-import IterativeArrayMethodsPage from './pages/IterativeArrayMethodsPage.jsx';
-import FunctionsAreObjectsTooPage from './pages/FunctionsAreObjectsTooPage.jsx';
-import FloatingPointVisualisationPage from './pages/FloatingPointVisualisationPage.jsx';
-import BuildAGameWithTddPage from './pages/BuildAGameWithTddPage.jsx';
+import PageContentFactory from './PageContentFactory.jsx';
 
 const page_mapper = {
     Articles: {
-        content_tag: ArticlesPage,
+        content_component_name: "ArticlesPage",
         hero_img: "articles.jpg",
         title: "Articles",
     },
     Index: {
-        content_tag: IndexPage,
+        content_component_name: "IndexPage",
         hero_img: "index.jpg",
         title: "Joseph Sinfield",
     },
     BooleanFunction: {
-        content_tag: BooleanFunctionPage,
+        content_component_name: "BooleanFunctionPage",
         hero_img: "boolean-function.png",
         title: "The boolean function",
     },
     UltimatePseudoClassicalInheritance: {
-        content_tag: UltimatePseudoClassicalInheritancePage,
+        content_component_name: "UltimatePseudoClassicalInheritancePage",
         hero_img: "ultimate-pseudo-classical-inheritance.jpg",
         title: "The ultimate pseudo-classical inheritance pattern",
     },
     TheTwoPillarsOfJavascript: {
-        content_tag: TheTwoPillarsOfJavascriptPage,
+        content_component_name: "TheTwoPillarsOfJavascriptPage",
         hero_img: "the-two-pillars-of-javascript.jpg",
         title: "The two pillars of Javascript",
     },
     TheObjectObject: {
-        content_tag: TheObjectObjectPage,
+        content_component_name: "TheObjectObjectPage",
         hero_img: "the-object-object.jpg",
         title: "The Object object",
     },
     PrefixVsPostfixIncrementUnaryOperators: {
-        content_tag: PrefixVsPostfixIncrementUnaryOperatorsPage,
+        content_component_name: "PrefixVsPostfixIncrementUnaryOperatorsPage",
         hero_img: "prefix-vs-postfix-increment-unary-operators.jpg",
         title: "Prefix vs postfix increment unary operators",
     },
     NumberConversion: {
-        content_tag: NumberConversionPage,
+        content_component_name: "NumberConversionPage",
         hero_img: "number-conversion.jpg",
         title: "Number conversion",
     },
     ModularisationWithoutObstruction: {
-        content_tag: ModularisationWithoutObstructionPage,
+        content_component_name: "ModularisationWithoutObstructionPage",
         hero_img: "modularisation-without-obstruction.jpg",
         title: "Modularisation without obstruction",
     },
     LoopsOrArrayMethods: {
-        content_tag: LoopsOrArrayMethodsPage,
+        content_component_name: "LoopsOrArrayMethodsPage",
         hero_img: "loops-or-array-methods.jpg",
         title: "Loops or array-methods",
     },
     KeywordsAndReservedWordsInJavascript: {
-        content_tag: KeywordsAndReservedWordsInJavascriptPage,
+        content_component_name: "KeywordsAndReservedWordsInJavascriptPage",
         hero_img: "keywords-and-reserved-words-in-javascript.jpg",
         title: "Keywords & reserved words in Javascript",
     },
     IterativeArrayMethods: {
-        content_tag: IterativeArrayMethodsPage,
+        content_component_name: "IterativeArrayMethodsPage",
         hero_img: "iterative-array-methods.jpg",
         title: "Iterative array methods",
     },
     FunctionsAreObjectsToo: {
-        content_tag: FunctionsAreObjectsTooPage,
+        content_component_name: "FunctionsAreObjectsTooPage",
         hero_img: "functions-are-objects-too.jpeg",
         title: "Functions are objects too",
     },
     FloatingPointVisualisation: {
-        content_tag: FloatingPointVisualisationPage,
+        content_component_name: "FloatingPointVisualisationPage",
         hero_img: "floating-point-visualisation.jpg",
         title: "Floating point visualisation",
     },
     BuildAGameWithTdd: {
-        content_tag: BuildAGameWithTddPage,
+        content_component_name: "BuildAGameWithTddPage",
         hero_img: "build-a-game-with-tdd.jpg",
         title: "Build a game with TDD",
     },
@@ -119,11 +104,10 @@ class PageBody extends React.Component {
     render() {
         const pathname = window.location.pathname;
         const mapped_page = page_mapper[this.state.page_component];
-        const ContentTag = mapped_page.content_tag;
         return <div>
             <NavigationBar page_title={mapped_page.title} />
             <HeroBanner hero_img={mapped_page.hero_img} />
-            <ContentTag />
+            <PageContentFactory component_name={mapped_page.content_component_name} />
             <Footer />
         </div>
     };
--- src/react-components/PageBodyFactory.jsx
+++
@@ -1,9 +0,0 @@
-class PageBodyFactory extends React.Component {
-    render() {
-        return <div>
-            <ContentTag />
-            </div>
-    };
-}
-
-export default PageBodyFactory;
