9c606b910552c3f25a9debe2975796ee0b590f1d
js-jslog <jhs4jbs@hotmail.co.uk>
2018-02-04 01:33:12 +0000

    using a modular sizing scale. looks a bit messy though

--- src/react-components/muiThemes/JsLogMuiThemeGreyscale.js
+++ src/react-components/muiThemes/JsLogMuiThemeGreyscale.js
@@ -1,31 +1,47 @@
 import {createMuiTheme} from 'material-ui/styles';
 import red from 'material-ui/colors/red';
 
-// I NEED TO PICK SIZES WHICH HAVE SOME RHYTHEM BETWEEN THEM
+const size_root_p16 = '206.258rem';
+const size_root_p15 = '1363.571rem';
+const size_root_p14 = '842.751rem';
+const size_root_p13 = '520.86rem';
+const size_root_p12 = '321.916rem';
+const size_root_p11 = '198.959rem';
+const size_root_p10 = '122.966rem';
+const size_root_p9 = '75.999rem';
+const size_root_p8 = '46.971rem';
+const size_root_p7 = '29.03rem';
+const size_root_p6 = '17.942rem';
+const size_root_p5 = '11.089rem';
+const size_root_p4 = '6.854rem';
+const size_root_p3 = '4.236rem';
+const size_root_p2 = '2.618rem';
+const size_root_p1 = '1.618rem';
+const size_root = '1rem';
+const size_root_m1 = '0.618rem';
+const size_root_m2 = '0.382rem';
+const size_root_m3 = '0.236rem';
+const size_root_m4 = '0.146rem';
+const size_root_m5 = '0.09rem';
+const size_root_m6 = '0.056rem';
+
 let JsLogMuiTheme = createMuiTheme({
     typography: {
-        paragraph_verticle_spacing_xs: 20,
-        paragraph_verticle_spacing_sm: 30,
-        paragraph_verticle_spacing_md: 40,
-        paragraph_verticle_spacing_lg: 50,
-        paragraph_verticle_spacing_xl: 60,
+        margin_thin: size_root_p1,
+        margin_thick: size_root_p3,
+        line_height: size_root_p1,
     },
     layout: {
-        page_gutter_xs: '5px',
-        page_gutter_sm: '50px',
-        page_gutter_md: '100px',
-        page_gutter_lg: '300px',
-        page_gutter_xl: '450px',
-        figure_gutter_xs: '0px',
-        figure_gutter_sm: '0px',
-        figure_gutter_md: '100px',
-        figure_gutter_lg: '300px',
-        figure_gutter_xl: '450px',
-        banner_image_height_xs: '400px',
-        banner_image_height_sm: '500px',
-        banner_image_height_md: '600px',
-        banner_image_height_lg: '800px',
-        banner_image_height_xl: '900px',
+        page_gutter_xs: size_root_m1,
+        page_gutter_sm: size_root_p2,
+        page_gutter_md: size_root_p4,
+        page_gutter_lg: size_root_p6,
+        page_gutter_xl: size_root_p7,
+        banner_image_height_xs: size_root_p7,
+        banner_image_height_sm: size_root_p8,
+        banner_image_height_md: size_root_p8,
+        banner_image_height_lg: size_root_p9,
+        banner_image_height_xl: size_root_p10,
     },
     palette: {
         primary: {
@@ -52,126 +68,57 @@ let JsLogMuiTheme = createMuiTheme({
     overrides: {},
 });
 
-JsLogMuiTheme.layout = {
-    banner_image: {
-        height: JsLogMuiTheme.layout.banner_image_height_xs,
-        [JsLogMuiTheme.breakpoints.up('sm')]: {
-            height: JsLogMuiTheme.layout.banner_image_height_sm,
-        },
-        [JsLogMuiTheme.breakpoints.up('md')]: {
-            height: JsLogMuiTheme.layout.banner_image_height_md,
-        },
-        [JsLogMuiTheme.breakpoints.up('lg')]: {
-            height: JsLogMuiTheme.layout.banner_image_height_lg,
-        },
-        [JsLogMuiTheme.breakpoints.up('xl')]: {
-            height: JsLogMuiTheme.layout.banner_image_height_xl,
-        },
+JsLogMuiTheme.layout.banner_image = {
+    height: JsLogMuiTheme.layout.banner_image_height_xs,
+    [JsLogMuiTheme.breakpoints.up('sm')]: {
+        height: JsLogMuiTheme.layout.banner_image_height_sm,
     },
-    body_text: {
-        marginLeft: JsLogMuiTheme.layout.page_gutter_xs,
-        marginRight: JsLogMuiTheme.layout.page_gutter_xs,
-        [JsLogMuiTheme.breakpoints.up('sm')]: {
-            marginLeft: JsLogMuiTheme.layout.page_gutter_sm,
-            marginRight: JsLogMuiTheme.layout.page_gutter_sm,
-        },
-        [JsLogMuiTheme.breakpoints.up('md')]: {
-            marginLeft: JsLogMuiTheme.layout.page_gutter_md,
-            marginRight: JsLogMuiTheme.layout.page_gutter_md,
-        },
-        [JsLogMuiTheme.breakpoints.up('lg')]: {
-            marginLeft: JsLogMuiTheme.layout.page_gutter_lg,
-            marginRight: JsLogMuiTheme.layout.page_gutter_lg,
-        },
-        [JsLogMuiTheme.breakpoints.up('xl')]: {
-            marginLeft: JsLogMuiTheme.layout.page_gutter_xl,
-            marginRight: JsLogMuiTheme.layout.page_gutter_xl,
-        },
+    [JsLogMuiTheme.breakpoints.up('md')]: {
+        height: JsLogMuiTheme.layout.banner_image_height_md,
     },
-    figure: {
-        marginLeft: JsLogMuiTheme.layout.figure_gutter_xs,
-        marginRight: JsLogMuiTheme.layout.figure_gutter_xs,
-        [JsLogMuiTheme.breakpoints.up('sm')]: {
-            marginLeft: JsLogMuiTheme.layout.figure_gutter_sm,
-            marginRight: JsLogMuiTheme.layout.figure_gutter_sm,
-        },
-        [JsLogMuiTheme.breakpoints.up('md')]: {
-            marginLeft: JsLogMuiTheme.layout.figure_gutter_md,
-            marginRight: JsLogMuiTheme.layout.figure_gutter_md,
-        },
-        [JsLogMuiTheme.breakpoints.up('lg')]: {
-            marginLeft: JsLogMuiTheme.layout.figure_gutter_lg,
-            marginRight: JsLogMuiTheme.layout.figure_gutter_lg,
-        },
-        [JsLogMuiTheme.breakpoints.up('xl')]: {
-            marginLeft: JsLogMuiTheme.layout.figure_gutter_xl,
-            marginRight: JsLogMuiTheme.layout.figure_gutter_xl,
-        },
+    [JsLogMuiTheme.breakpoints.up('lg')]: {
+        height: JsLogMuiTheme.layout.banner_image_height_lg,
+    },
+    [JsLogMuiTheme.breakpoints.up('xl')]: {
+        height: JsLogMuiTheme.layout.banner_image_height_xl,
     },
 };
+JsLogMuiTheme.layout.responsive_page_gutter = {
+    marginLeft: JsLogMuiTheme.layout.page_gutter_xs,
+    marginRight: JsLogMuiTheme.layout.page_gutter_xs,
+    [JsLogMuiTheme.breakpoints.up('sm')]: {
+        marginLeft: JsLogMuiTheme.layout.page_gutter_sm,
+        marginRight: JsLogMuiTheme.layout.page_gutter_sm,
+    },
+    [JsLogMuiTheme.breakpoints.up('md')]: {
+        marginLeft: JsLogMuiTheme.layout.page_gutter_md,
+        marginRight: JsLogMuiTheme.layout.page_gutter_md,
+    },
+    [JsLogMuiTheme.breakpoints.up('lg')]: {
+        marginLeft: JsLogMuiTheme.layout.page_gutter_lg,
+        marginRight: JsLogMuiTheme.layout.page_gutter_lg,
+    },
+    [JsLogMuiTheme.breakpoints.up('xl')]: {
+        marginLeft: JsLogMuiTheme.layout.page_gutter_xl,
+        marginRight: JsLogMuiTheme.layout.page_gutter_xl,
+    },
+};
+JsLogMuiTheme.layout.body_text = JsLogMuiTheme.layout.responsive_page_gutter;
+JsLogMuiTheme.layout.figure = JsLogMuiTheme.layout.responsive_page_gutter;
 
+JsLogMuiTheme.typography.body_text_spacing = {
+    marginTop: JsLogMuiTheme.typography.margin_thin,
+    marginBottom: JsLogMuiTheme.typography.margin_thin,
+    lineHeight: JsLogMuiTheme.typography.line_height,
+};
+JsLogMuiTheme.typography.heading_text_spacing = {
+    marginTop: JsLogMuiTheme.typography.margin_thick,
+    marginBottom: JsLogMuiTheme.typography.margin_thin,
+};
 JsLogMuiTheme.overrides.MuiTypography = {
-    body1: {
-        marginTop: JsLogMuiTheme.typography.paragraph_verticle_spacing_xs + 'px',
-        marginBottom: JsLogMuiTheme.typography.paragraph_verticle_spacing_xs + 'px',
-        [JsLogMuiTheme.breakpoints.up('sm')]: {
-            marginTop: JsLogMuiTheme.typography.paragraph_verticle_spacing_sm + 'px',
-            marginBottom: JsLogMuiTheme.typography.paragraph_verticle_spacing_sm + 'px',
-        },
-        [JsLogMuiTheme.breakpoints.up('md')]: {
-            marginTop: JsLogMuiTheme.typography.paragraph_verticle_spacing_md + 'px',
-            marginBottom: JsLogMuiTheme.typography.paragraph_verticle_spacing_md + 'px',
-        },
-        [JsLogMuiTheme.breakpoints.up('lg')]: {
-            marginTop: JsLogMuiTheme.typography.paragraph_verticle_spacing_lg + 'px',
-            marginBottom: JsLogMuiTheme.typography.paragraph_verticle_spacing_lg + 'px',
-        },
-        [JsLogMuiTheme.breakpoints.up('xl')]: {
-            marginTop: JsLogMuiTheme.typography.paragraph_verticle_spacing_xl + 'px',
-            marginBottom: JsLogMuiTheme.typography.paragraph_verticle_spacing_xl + 'px',
-        },
-    },
-    display1: {
-        marginTop: (JsLogMuiTheme.typography.paragraph_verticle_spacing_xs * 2) + 'px',
-        marginBottom: JsLogMuiTheme.typography.paragraph_verticle_spacing_xs + 'px',
-        [JsLogMuiTheme.breakpoints.up('sm')]: {
-            marginTop: (JsLogMuiTheme.typography.paragraph_verticle_spacing_sm * 2) + 'px',
-            marginBottom: JsLogMuiTheme.typography.paragraph_verticle_spacing_sm + 'px',
-        },
-        [JsLogMuiTheme.breakpoints.up('md')]: {
-            marginTop: (JsLogMuiTheme.typography.paragraph_verticle_spacing_md * 2) + 'px',
-            marginBottom: JsLogMuiTheme.typography.paragraph_verticle_spacing_md + 'px',
-        },
-        [JsLogMuiTheme.breakpoints.up('lg')]: {
-            marginTop: (JsLogMuiTheme.typography.paragraph_verticle_spacing_lg * 2) + 'px',
-            marginBottom: JsLogMuiTheme.typography.paragraph_verticle_spacing_lg + 'px',
-        },
-        [JsLogMuiTheme.breakpoints.up('xl')]: {
-            marginTop: (JsLogMuiTheme.typography.paragraph_verticle_spacing_xl * 2) + 'px',
-            marginBottom: JsLogMuiTheme.typography.paragraph_verticle_spacing_xl + 'px',
-        },
-    },
-    title: {
-        marginTop: (JsLogMuiTheme.typography.paragraph_verticle_spacing_xs * 2) + 'px',
-        marginBottom: JsLogMuiTheme.typography.paragraph_verticle_spacing_xs + 'px',
-        [JsLogMuiTheme.breakpoints.up('sm')]: {
-            marginTop: (JsLogMuiTheme.typography.paragraph_verticle_spacing_sm * 2) + 'px',
-            marginBottom: JsLogMuiTheme.typography.paragraph_verticle_spacing_sm + 'px',
-        },
-        [JsLogMuiTheme.breakpoints.up('md')]: {
-            marginTop: (JsLogMuiTheme.typography.paragraph_verticle_spacing_md * 2) + 'px',
-            marginBottom: JsLogMuiTheme.typography.paragraph_verticle_spacing_md + 'px',
-        },
-        [JsLogMuiTheme.breakpoints.up('lg')]: {
-            marginTop: (JsLogMuiTheme.typography.paragraph_verticle_spacing_lg * 2) + 'px',
-            marginBottom: JsLogMuiTheme.typography.paragraph_verticle_spacing_lg + 'px',
-        },
-        [JsLogMuiTheme.breakpoints.up('xl')]: {
-            marginTop: (JsLogMuiTheme.typography.paragraph_verticle_spacing_xl * 2) + 'px',
-            marginBottom: JsLogMuiTheme.typography.paragraph_verticle_spacing_xl + 'px',
-        },
-    },
+    body1: JsLogMuiTheme.typography.body_text_spacing,
+    title: JsLogMuiTheme.typography.heading_text_spacing,
+    display1: JsLogMuiTheme.typography.heading_text_spacing,
 };
-
 
 export default JsLogMuiTheme;
