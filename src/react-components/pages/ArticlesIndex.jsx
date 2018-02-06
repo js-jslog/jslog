bdca318e40af23611b08067fbd74d6eb1477caf8
js-jslog <josephsinfield@yahoo.com>
2018-02-06 13:49:03 +0000

    making the grid layout depend on state

--- src/react-components/pages/ArticlesIndex.jsx
+++ src/react-components/pages/ArticlesIndex.jsx
@@ -16,28 +16,37 @@ const styles = {
     },
 };
 
-const ArticlesGridList = (props) => (
-    <div>
-        <GridList
-            cellHeight={180}
-        >
-            {articles_list.map((tile) => (
-                <GridListTile
-                    key={tile.image}
-                >
-                    <Link to={"/articles/" + tile.link} >
-                        <img className={props.classes.grid_image} src={"images/hero/" + tile.image} href={"articles/" + tile.link} />
-                        <GridListTileBar
-                            title={tile.title}
-                            subtitle={tile.blurb}
-                        >
-                        </GridListTileBar>
-                    </Link>
-                </GridListTile>
-            ))}
-        </GridList>
-    </div>
-);
+class ArticlesGridList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            cols: 4,
+        };
+    };
+    render () {
+        return (
+            <GridList
+                cellHeight={180}
+                cols={this.state.cols}
+            >
+                {articles_list.map((tile) => (
+                    <GridListTile
+                        key={tile.image}
+                    >
+                        <Link to={"/articles/" + tile.link} >
+                            <img className={this.props.classes.grid_image} src={"images/hero/" + tile.image} href={"articles/" + tile.link} />
+                            <GridListTileBar
+                                title={tile.title}
+                                subtitle={tile.blurb}
+                            >
+                            </GridListTileBar>
+                        </Link>
+                    </GridListTile>
+                ))}
+            </GridList>
+        );
+    };
+};
 
 const ArticlesIndex = (props) => (
     <div>
