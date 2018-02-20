b9d0aa6664c2a03d95578b583bb04aeaf3d6e792
js-jslog <josephsinfield@yahoo.com>
2018-02-20 14:20:12 +0000

    partially completed the number conversion page

--- src/react-components/pages/articles/NumberConversion.jsx
+++ src/react-components/pages/articles/NumberConversion.jsx
@@ -1,6 +1,122 @@
 import React from 'react';
 import {withStyles} from 'material-ui/styles';
 
+import CodeBlock, {CodeBlockOutput, CodeBlockErrorOutput} from '../../layout/code/CodeBlock.jsx';
+import Code from '../../layout/code/Code.jsx';
+import Figure, {FigureCaption} from '../../layout/figure/Figure.jsx';
+import Typography from 'material-ui/Typography';
+import Table, { TableBody, TableCell, TableHead, TableRow } from 'material-ui/Table';
+import Paper from 'material-ui/Paper';
+import {Link} from 'react-router-dom';
+import Grid from 'material-ui/Grid';
+import List, {ListItem} from 'material-ui/List';
+
+let id = 0;
+function createData(input, rule, eg_in, eg_out) {
+      id += 1;
+      return {id, input, rule, eg_in, eg_out}
+}
+
+const number_conversion_data = [
+    createData('Number', 'Number types (apart from NaN - see below) are passed straight through the function unaffected', 'Number(5)', '5'),
+    createData('Number', 'Number types (apart from NaN - see below) are passed straight through the function unaffected', 'Number(Infinity)', 'Infinity'),
+    createData('Number', 'Number types (apart from NaN - see below) are passed straight through the function unaffected', 'Number(NaN)', 'NaN'),
+    createData('Boolean', 'true and false are converted to 1 & 0 respectively', 'Number(true)', '1'),
+    createData('Boolean', 'true and false are converted to 1 & 0 respectively', 'Number(false)', '0'),
+    createData('null', 'Returns 0', 'Number(null)', '0'),
+    createData('undefined', 'Returns NaN', 'Number(undefined)', 'NaN'),
+    createData('Strings', 'Strings containing only numbers will always be converted to decimals. Signs are accounted for & leading zeros are ignored', 'Number(\'1\')', '1'),
+    createData('Strings', 'Strings containing only numbers will always be converted to decimals. Signs are accounted for & leading zeros are ignored', 'Number(\'-1\')', '-1'),
+    createData('Strings', 'Strings containing only numbers will always be converted to decimals. Signs are accounted for & leading zeros are ignored', 'Number(\'0001\')', '1'),
+    createData('Strings', 'Strings containing valid floating-point formats are converted accordingly', 'Number(\'1.1\')', '1.1'),
+    createData('Strings', 'Strings containing valid floating-point formats are converted accordingly', 'Number(\'0001.1\')', '1.1'),
+    createData('Strings', 'Valid hexadecimal formats beginning with 0x will be converted to the appropriate integer', 'Number(\'0x1\')', '1'),
+    createData('Strings', 'Empty Strings are converted to 0', 'Number(\'\')', '0'),
+    createData('Strings', 'Any formates not matching these String rules will be converted to NaN', 'Number(\'true\')', 'NaN'),
+    createData('All', 'All white space before and after the evaluated parameter is ignored. Surprisingly, this includes white space after the sign if included', 'Number( 1 )', '1'),
+    createData('All', 'All white space before and after the evaluated parameter is ignored. Surprisingly, this includes white space after the sign if included', 'Number( - 1 )', '-1'),
+    createData('All', 'All white space before and after the evaluated parameter is ignored. Surprisingly, this includes white space after the sign if included', 'Number( \'   \'    )', '0'),
+    createData('All', 'All white space before and after the evaluated parameter is ignored. Surprisingly, this includes white space after the sign if included', 'Number( \'   0x1 \'    )', '1'),
+    createData('All', 'Anything not matching any of the rules above will throw a syntax error', 'Number( 5 5 )', 'Uncaught SyntaxError: missing ) after argument list'),
+];
+
+const parseint_conversion_data = [
+    createData('Strings', '1. Leading white space and 0\'s are ignored', 'parseInt(\'    001\')', '1'),
+    createData('Strings', '2. If the first character isn\'t a number or a sign, a NaN is returned', 'parseInt(\'    .1\')', 'NaN'),
+    createData('Strings', '2. If the first character isn\'t a number or a sign, a NaN is returned', 'parseInt(\'\')', 'NaN (Note: different to Number)'),
+    createData('Strings', '3. If the first character is a number or sign, the string is evaluated until either the end of the string, or the first non-numeric character (excpet in the special cases below', 'parseInt(\'    +1\')', '1'),
+    createData('Strings', '3. If the first character is a number or sign, the string is evaluated until either the end of the string, or the first non-numeric character (excpet in the special cases below', 'parseInt(\'    +1test\')', '1'),
+    createData('Strings', '3. If the first character is a number or sign, the string is evaluated until either the end of the string, or the first non-numeric character (excpet in the special cases below', 'parseInt(\'1.1\')', '1 (Note: . is an invalid character in the parseInt sequence'),
+    createData('Strings', 'Special cases exist which break the above rules to some extent','',''), 
+    createData('Strings', 'Step 3 behaves differently if the first 2 characters are 0x. The rest of the string is evaluated as a hexadecimal until the first non-hexadecimal character is met, at which point it stops parsing like in step 3','parseInt(\'0x1\')','1'), 
+    createData('Strings', 'Step 3 behaves differently if the first 2 characters are 0x. The rest of the string is evaluated as a hexadecimal until the first non-hexadecimal character is met, at which point it stops parsing like in step 3','parseInt(\'0x1test\'','1'), 
+    createData('Strings', 'If the number looks like an octal literal then in ECMAScript3 it will be interpreted as such, while in ECMAScript5 it will be interpreted as a decimal (therefore according to the standard rules)','',''), 
+    createData('Strings', 'ECMAScript5','parseInt(\'010\')','10'), 
+    createData('Strings', 'ECMAScript3','parseInt(\'010\')','8'),
+];
+
+const NumberConversionTable = function () {
+    return (
+        <Figure>
+            <Paper>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                        <TableCell>Input</TableCell>
+                        <TableCell>Rule Applied</TableCell>
+                        <TableCell>Examples : IN</TableCell>
+                        <TableCell>OUT</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                    {number_conversion_data.map(n => {
+                    return (
+                        <TableRow key={n.id}>
+                            <TableCell>{n.input}</TableCell>
+                            <TableCell>{n.rule}</TableCell>
+                            <TableCell>{n.eg_in}</TableCell>
+                            <TableCell>{n.eg_out}</TableCell>
+                        </TableRow>
+                    );
+                    })}
+                    </TableBody>
+                </Table>
+            </Paper>
+        </Figure>
+    );
+}
+
+const ParseIntConversionTable = function () {
+  return (
+        <Figure>
+            <Paper>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                        <TableCell>Input</TableCell>
+                        <TableCell>Rule Applied</TableCell>
+                        <TableCell>Examples : IN</TableCell>
+                        <TableCell>OUT</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                    {parseint_conversion_data.map(n => {
+                    return (
+                        <TableRow key={n.id}>
+                            <TableCell>{n.input}</TableCell>
+                            <TableCell>{n.rule}</TableCell>
+                            <TableCell>{n.eg_in}</TableCell>
+                            <TableCell>{n.eg_out}</TableCell>
+                        </TableRow>
+                    );
+                    })}
+                    </TableBody>
+                </Table>
+            </Paper>
+        </Figure>
+    );
+}
+
 const styles = theme => ({
 });
 
@@ -11,13 +127,14 @@ const blurb = "A look at number conversion in JavaScript";
 const published = true;
 
 const PageContents = () => (
-      <div className="container">
-         <div className="section">
-<p>
+  <div>
+    <Typography>
 Type conversion is at the heart of JavaScript's flexibility. The earlier article on the type conversion implicit in flow control statements discussed the <a href="boolean_function">Boolean() casting function</a> demonstrated how various types are converted to the boolean type. Converting to the numeric type is one of the most common conversions you will come across in JavaScript development. However despite its ease of use, it is not necessarily as straight forward as it appears.
-</p>
-<h2>3 Functions</h2>
-<p>
+</Typography>
+<Typography variant='display1'>
+  3 Functions
+</Typography>
+<Typography>
 There are 3 functions with which you can convert to numeric values :
 <ul>
 <li>Number()</li>
@@ -25,208 +142,36 @@ There are 3 functions with which you can convert to numeric values :
 <li>parseFloat()</li>
 </ul>
 The rules governing each of these may be most simply understood in the following tables.
-</p>
-<h3>Number()</h3>
-<p>
-<figure>
-<table className="table">
-<tbody>
- <tr>
-  <th>Input</th>
-  <th>Rule Applied</th>
-  <th>Examples : IN</th>
-  <th>OUT</th>
- </tr>
- <tr>
-  <td rowspan="3">Number</td>
-  <td rowspan="3">Number types (apart from NaN - see below) are passed straight through the function unaffected</td>
-  <td><code>Number(5)</code></td>
-  <td><output>5</output></td>
- </tr>
- <tr>
-  <td><code>Number(Infinity)</code></td>
-  <td><output>Infinity</output></td>
- </tr>
- <tr>
-  <td><code>Number(NaN)</code></td>
-  <td><output>NaN</output></td>
- </tr>
- <tr>
-  <td rowspan="2">Boolean</td>
-  <td rowspan="2"><code>true</code> and <code>false</code> are converted to <code>1</code> & <code>0</code> respectively</td>
-  <td><code>Number(true)</code></td>
-  <td><output>1</output></td>
- </tr>
- <tr>
-  <td><code>Number(false)</code></td>
-  <td><output>0</output></td>
- </tr>
- <tr>
-  <td><code>null</code></td>
-  <td>Returns 0</td>
-  <td><code>Number(null)</code></td>
-  <td><output>0</output></td>
- </tr>
- <tr>
-  <td><code>undefined</code></td>
-  <td>Returns <code>NaN</code></td>
-  <td><code>Number(undefined)</code></td>
-  <td><output><code>NaN</code></output></td>
- </tr>
- <tr>
-  <td rowspan="8"><code>Strings</code></td>
-  <td rowspan="3">Strings containing only numbers will always be converted to decimals. Signs are accounted for & leading zeros are ignored.</td>
-  <td><code>Number("1")</code></td>
-  <td><output>1</output></td>
- </tr>
- <tr>
-  <td><code>Number("-1")</code></td>
-  <td><output>-1</output></td>
- </tr>
- <tr>
-  <td><code>Number("0001")</code></td>
-  <td><output>1</output></td>
- </tr>
- <tr>
-  <td rowspan="2">Strings containing valid floating-point formats are converted accordingly</td>
-  <td><code>Number("1.1")</code></td>
-  <td><output>1.1</output></td>
- </tr>
- <tr>
-  <td><code>Number("0001.1")</code></td>
-  <td><output>1.1</output></td>
- </tr>
- <tr>
-  <td>Valid hexadecimal formats beginning with <code>0x</code> will be converted to the appropriate integer</td>
-  <td><code>Number("0x1")</code></td>
-  <td><output>1</output></td>
- </tr>
- <tr>
-  <td>Empty Strings are converted to 0</td>
-  <td><code>Number("")</code></td>
-  <td><output>0</output></td>
- </tr>
- <tr>
-  <td>Any formats not matching these String rules will be converted to <code>NaN</code></td>
-  <td><code>Number( "true" )</code></td>
-  <td><output>NaN</output></td>
- </tr>
- <tr>
-  <td rowspan="5">All</td>
-  <td rowspan="4">All white space before and after the evaluated parameter is ignored. Surprisingly this includes white space after the sign if included.</td>
-  <td><code>Number(  1  )</code></td>
-  <td><output>1</output></td>
- </tr>
- <tr>
-  <td><code>Number( - 1 )</code></td>
-  <td><output>-1</output></td>
- </tr>
- <tr>
-  <td><code>Number( "    "   )</code></td>
-  <td><output>0</output></td>
- </tr>
- <tr>
-  <td><code>Number( "   0x1 "   )</code></td>
-  <td><output>1</output></td>
- </tr>
- <tr>
-  <td>Anything not matching any of the rules above will throw a syntax error</td>
-  <td><code>Number( 5 5)</code></td>
-  <td><output className="error">Uncaught SyntaxError: missing ) after argument list</output></td>
- </tr>
-</tbody>
-</table>
-<figcaption>Rules for converting various types to numbers using the Number() function</figcaption>
-</figure>
-</p>
-<p>
+</Typography>
+<Typography variant='display1'>
+  Number()
+</Typography>
+  <Figure>
+    <NumberConversionTable />
+    <FigureCaption>
+      Rules for converting various types to numbers using the Number() function
+    </FigureCaption>
+  </Figure>
+  <Typography>
 We are about to see a pair of much more reliable conversion functions. However it is important to understand the Number() function as <i>it is the function which defines the auto-casting to numbers when using bitwise operators</i>.
-</p>
-<h3>parseInt()</h3>
-<p>
+</Typography>
+<Typography variant='display1'>
+  parseInt()
+</Typography>
+<Typography>
 The parsInt() function behaves the same as the Number() function, other than it's effect on strings. For strings it will convert according to the following rules:
 <detail>The rules are applied in the order listed. If a rule does not apply, the next rule will be applied.</detail>
-</p>
-<p>
-<figure>
-<table className="table">
-<tbody>
- <tr>
-  <th>Input</th>
-  <th>Rule Applied</th>
-  <th>Examples : IN</th>
-  <th>OUT</th>
- </tr>
- <tr>
-  <td rowspan="13"><code>Strings</code></td>
-  <td>1. Leading white space and 0's are ignored</td>
-  <td><code>parseInt("   001")</code></td>
-  <td><output>1</output></td>
- </tr>
- <tr>
-  <td rowspan="2">2. If the first character isn't a number or a sign, a <code>NaN</code> is returned</td>
-  <td><code>parseInt("  .1")</code></td>
-  <td><output>NaN</output></td>
- </tr>
- <tr>
-  <td><code>parseInt("")</code></td>
-  <td><output>NaN</output> Note : different to Number()</td>
- </tr>
- <tr>
-  <td rowspan="3">3. If the first character is a number or sign, the string is evaluated until either the end of the string, or the first non-numeric character (except in the special cases below)</td>
-  <td><code>parseInt("  +1")</code></td>
-  <td><output>111</output></td>
- </tr>
- <tr>
-  <td><code>parseInt("  +1test")</code></td>
-  <td><output>1</output></td>
- </tr>
- <tr>
-  <td><code>parseInt("1.1")</code></td>
-  <td><output>1</output> Note: . is an invalid character in the parseInt() sequence</td>
- </tr>
- <tr>
-  <td colspan="3">
-    <b>Special cases exist which break the above rules to some extent</b>
-  </td>
- </tr>
- <tr>
-  <td rowspan="2">
-    Step 3 behaves differently if the first 2 characters are 0x. The rest of the string is evaluated as a hexadecimal until the first non-hexadecimal character is met, at which point it stops parsing like in step 3
-  </td>
-  <td><code>parseInt("0x1")</code></td>
-  <td><output>1</output></td>
- </tr>
- <tr>
-  <td><code>parseInt("0x1test")</code></td>
-  <td><output>1</output></td>
- </tr>
- <tr>
-  <td rowspan="4">
-    If the number looks like an octal literal then in ECMAScript3 it will be interpreted as such, while in ECMAScript5 it will be interpreted as a decimal (therefore according to the standard rules)
-  </td>
-  <td colspan="2">ECMAScript5</td>
- </tr>
- <tr>
-  <td><code>parseInt("010")</code></td>
-  <td><output>10</output></td>
- </tr>
- <tr>
-  <td colspan="2">ECMAScript3</td>
- </tr>
- <tr>
-  <td><code>parseInt("010")</code></td>
-  <td><output>8</output></td>
- </tr>
-</tbody>
-</table>
-<figcaption>Rules for converting various types to numbers using the parseInt() function</figcaption>
-</figure>
-</p>
-<p>
+  </Typography>
+  <Figure>
+    <ParseIntConversionTable />
+    <FigureCaption>
+      Rules for converting various types to numbers using the parseInt() function
+    </FigureCaption>
+  </Figure>
+  <Typography>
 As you can see there is a lot of potential confusion involved in using parseInt() with number formats which are up for interpretation. To avoid this & because you are likely to know what format your number should be coming in, even if it might look like another format, an additional parameter is available for the parseInt() function. This parameter enables you to define what base the number coming in should be interpreted in. It is called the <i>radix</i>.
-</p>
-<p>
+</Typography>
+  <Typography>
 <figure>
 <table className="table">
 <tbody>
@@ -259,12 +204,14 @@ As you can see there is a lot of potential confusion involved in using parseInt(
 </table>
 <figcaption>The effect of the <i>radix</i> on parseInt() string interpretation</figcaption>
 </figure>
-</p>
-<h3>parseFloat()</h3>
-<p>
+  </Typography>
+  <Typography variant='display1'>
+    parseFloat()
+  </Typography>
+<Typography>
 The parseFloat() function operates in the same way as parseInt() except that it has <b>no radix parameter</b>, only accepts <b>decimal form</b> at at stage 3 it will also accept a <b>single decimal point</b> followed once again only by numeric characters.
-</p>
-<p>
+</Typography>
+  <Typography>
 <figure>
 <table className="table">
 <tbody>
@@ -300,9 +247,8 @@ The parseFloat() function operates in the same way as parseInt() except that it 
 </table>
 <figcaption>The use of decimal points in the parseFloat() function</figcaption>
 </figure>
-</p>
+  </Typography>
           </div>
-      </div>
 );
 
 const StyledPageContents = withStyles(styles)(PageContents);
