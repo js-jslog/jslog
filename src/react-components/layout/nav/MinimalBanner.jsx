74a7266011335fd629270199b973c3afadf7b31a
js-jslog <josephsinfield@yahoo.com>
2018-03-10 18:20:58 +0000

    a slight modification to the minimal banner. it's still not even a concept

--- src/react-components/layout/nav/MinimalBanner.jsx
+++ src/react-components/layout/nav/MinimalBanner.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import AppBar from 'material-ui/AppBar';
 import Typography from 'material-ui/Typography';
+import {Link} from 'react-router-dom';
 
 import {withStyles} from 'material-ui/styles';
 
-const styles = theme => ({});
+const styles = theme => ({
+    nav: {
+        color: theme.palette.primary.main,
+    }
+});
 
 const MinimalBanner = function (props) { 
     const {classes} = props;
     return (
-        <AppBar position='static'>
-            This is my site
-        </AppBar>
+        <div>
+            <AppBar className={classes.nav} position='static'>
+                JS
+            </AppBar>
+            <Link to='/'>
+                Home
+            </Link>
+        </div>
     );
 };
 
