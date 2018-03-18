6e7424b087b3c0b0e9857b8ab86377f189e13033
js-jslog <josephsinfield@yahoo.com>
2018-03-18 16:37:01 +0000

    shouldn't use aliases in bash scripts

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
-rm-docker-containers
+sudo docker stop running-jslog
+sudo docker ps -aq --no-trunc -f status=exited | sudo xargs docker rm
 sudo docker build -t jslog-image .
 sudo docker run -d --name running-jslog -p 80:3000 jslog-image
