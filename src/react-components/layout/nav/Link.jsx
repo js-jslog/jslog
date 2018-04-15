7650953b623ba40e1fe62a06369239b3612190be
js-jslog <jhs4jbs@hotmail.co.uk>
2018-04-15 18:34:30 +0100

    acknowledging the original author for the Link component

--- src/react-components/layout/nav/Link.jsx
+++ src/react-components/layout/nav/Link.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {Link as ReactLink} from 'react-router';
 
+// courtesy of https://gist.github.com/shprink/bf9599e1d66b9dc4d151e89c1199ccb8
+
 export default class Link extends React.Component {
   parseTo(to) {
     let parser = document.createElement('a');
